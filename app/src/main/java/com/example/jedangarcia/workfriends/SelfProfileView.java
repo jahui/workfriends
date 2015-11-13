d1d99bbc4bec492c17c1bac9f6b413bf47a1f25d
kkim0211 <kkim0211@gmail.com>
2015-11-13 07:30:46 -0800

    kev's commit

--- app/src/main/java/com/example/jedangarcia/workfriends/SelfProfileView.java
+++ app/src/main/java/com/example/jedangarcia/workfriends/SelfProfileView.java
@@ -1,8 +1,19 @@
 package com.example.jedangarcia.workfriends;
 
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+
 /**
  * Created by jedan.garcia on 11/13/15.
  */
-public class SelfProfileView {
+public class SelfProfileView extends Activity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_self_profile);
+    }
 }
