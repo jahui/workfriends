df62644bef365fbca31619c00f6db553964fa4a7
jahui <j.aa.dahui@gmail.com>
2015-11-13 07:15:05 -0800

    MatchView class and the layout for it with hard coded images.

---
+++ app/src/main/java/com/example/jedangarcia/workfriends/MatchView.java
@@ -0,0 +1,110 @@
+package com.example.jedangarcia.workfriends;
+
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.widget.Toast;
+
+
+import com.example.jedangarcia.workfriends.R;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MatchView extends Activity {
+
+    private String matchURL = "http://geekfancy.com/workfriends2/get_match.php?uid=11&week=2015.46";
+    private final String DEBUG_TAG = "MatchView";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        String uidstring = intent.getStringExtra("uid");
+        String weekstring = intent.getStringExtra("week");
+
+        String stringUrl = matchURL + "uid=" + uidstring + "&week=" + weekstring;
+
+        class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+            @Override
+            protected String doInBackground(String... urls) {
+
+                // params comes from the execute() call: params[0] is the url.
+                try {
+                    return downloadUrl(urls[0]);
+                } catch (IOException e) {
+                    return "Unable to retrieve web page. URL may be invalid.";
+                }
+            }
+            // onPostExecute displays the results of the AsyncTask.
+            @Override
+            protected void onPostExecute(String result) {
+                // parse the JSON
+                Log.d(DEBUG_TAG, result);
+            }
+        }
+
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            new DownloadWebpageTask().execute(stringUrl);
+        } else {
+            Toast.makeText(getApplicationContext(), "No network connection available.", Toast.LENGTH_SHORT);
+        }
+
+        setContentView(R.layout.activity_match_view);
+    }
+    private String downloadUrl(String myurl) throws IOException {
+        InputStream is = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+        int len = 5000;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d(DEBUG_TAG, "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is, len);
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
+    }
+
+}
